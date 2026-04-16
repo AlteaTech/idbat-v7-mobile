f4549f12b6885db1867639bda728ac1d272881b1
Jeremie Vidal <jeremie.vidal13@gmail.com>
2026-04-16 16:30:58 +0200

    refonte du composant BottomLargeButton et ajout d'une icône
    - Mise à jour visuelle du bouton avec une hauteur augmentée (90dp) et une couleur rose corail.
    - Intégration d'une icône illustrative (camion) à droite du texte.
    - Utilisation d'une `Surface` cliquable pour permettre une mise en page personnalisée via une `Row`.

--- app/src/main/java/com/idbat/mobile/ui/components/BottomLargeButton.kt
+++ app/src/main/java/com/idbat/mobile/ui/components/BottomLargeButton.kt
@@ -1,36 +1,49 @@
 package com.idbat.mobile.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.idbat.mobile.R
 
 @Composable
-fun BottomLargeButton(
-    title: String,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Button(
-        onClick = onClick,
-        modifier = modifier
+fun BottomLargeButton(title: String, onClick: () -> Unit) {
+    Surface(
+        modifier = Modifier
             .fillMaxWidth()
-            .height(56.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Black
-        ),
-        shape = RoundedCornerShape(28.dp)
+            .height(90.dp)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(45.dp),
+        color = Color(0xFFF27059).copy(alpha = 0.8f) // Rose corail
     ) {
-        Text(
-            text = title,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Row(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Image(
+                painter = painterResource(id = R.drawable.truck2),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f),
+            )
+
+        }
     }
 }
