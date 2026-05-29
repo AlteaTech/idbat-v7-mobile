c79c52939c8bf502105e45602661265399517edc
Jeremie Vidal <jeremie.vidal13@gmail.com>
2026-05-29 14:35:39 +0200

    Set base URL to staging environment

--- app/src/main/java/com/idbat/mobile/singleton/ConfigSingleton.kt
+++ app/src/main/java/com/idbat/mobile/singleton/ConfigSingleton.kt
@@ -6,5 +6,5 @@ object ConfigSingleton {
     const val BASE_URL_DEV     = "http://10.0.2.2:8091/"
     const val BASE_URL_STAGING = "https://idbat-mobile-rec.recyclage.veolia.fr/"
 
-    val baseUrl = BASE_URL_DEV
+    val baseUrl = BASE_URL_STAGING
 }
