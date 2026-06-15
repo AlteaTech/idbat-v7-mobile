0524f9695b8c22fee6ccf6eeb62d74e44751a0c4
Jeremie Vidal <jeremie.vidal13@gmail.com>
2026-06-15 10:07:48 +0200

    mise en staging

--- app/src/main/java/com/idbat/mobile/singleton/ConfigSingleton.kt
+++ app/src/main/java/com/idbat/mobile/singleton/ConfigSingleton.kt
@@ -12,5 +12,5 @@ object ConfigSingleton {
     const val BASE_URL_DEV_DEVICE   = "http://localhost:8091/"   // nécessite : adb reverse tcp:8091 tcp:8091
     const val BASE_URL_STAGING      = "https://idbat-mobile-rec.recyclage.veolia.fr/"
 
-    val baseUrl = BASE_URL_DEV_DEVICE
+    val baseUrl = BASE_URL_STAGING
 }
