e0dd1e8d6dd147b2f218000c5c0a89353b58ed43
Jeremie Vidal <jeremie.vidal13@gmail.com>
2026-09-09 18:04:10 +0200

    fix wording

--- app/src/main/java/com/idbat/mobile/ui/screens/PassageInfoScreen.kt
+++ app/src/main/java/com/idbat/mobile/ui/screens/PassageInfoScreen.kt
@@ -345,7 +345,7 @@ private fun SeuilRow(seuil: SeuilEtatEntity) {
     val message = if (!seuil.isAlerte) {
         "Seuil ${seuil.nom} atteint, ${seuil.nbPassagesEffectues} passages ${periode} pour ${nbAutorises} autorisés"
     } else {
-        "Le seuil ${seuil.nom} autorise ${nbAutorises} passages ${periode}, vous en avez déjà effectué ${seuil.nbPassagesEffectues}"
+        "Le seuil ${seuil.nom} autorise ${nbAutorises} passages ${periode}, ${seuil.nbPassagesEffectues} déjà effectués"
     }
     Row(
         verticalAlignment = Alignment.Top,
